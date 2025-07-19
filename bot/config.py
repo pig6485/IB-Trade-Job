f4ef18e4043acf3c36ac5e66220425a5a84647b5
chunhao <pig6485@gmail.com>
2025-07-19 16:00:46 +0800

    feat: updated README file to English and created requirements.txt file

--- bot/config.py
+++ bot/config.py
@@ -1,6 +1,20 @@
 import os
+from dotenv import load_dotenv
 
-# 預設值，可用環境變數覆蓋
-IB_HOST = os.getenv("IB_HOST", "127.0.0.1")
-IB_PORT = int(os.getenv("IB_PORT", 4002))
-IB_CLIENT_ID = int(os.getenv("IB_CLIENT_ID", 1))
+load_dotenv()
+
+IB_HOST = os.environ.get("IB_HOST", "127.0.0.1")
+IB_PORT = int(os.environ.get("IB_PORT", 4002))
+IB_CLIENT_ID = int(os.environ.get("IB_CLIENT_ID", 1))
+MAILGUN_API_KEY = os.environ.get("MAILGUN_API_KEY")
+MAILGUN_DOMAIN = os.environ.get("MAILGUN_DOMAIN")
+MAILGUN_RECIPIENT = os.environ.get('MAILGUN_RECIPIENT')
+NEED_AMOUMT = os.environ.get('NEED_AMOUMT', 5000)
+
+print(f"IB_HOST: {IB_HOST}")
+print(f"IB_PORT: {IB_PORT}")
+print(f"IB_CLIENT_ID: {IB_CLIENT_ID}")
+print(f"MAILGUN_API_KEY: {MAILGUN_API_KEY}")
+print(f"MAILGUN_DOMAIN: {MAILGUN_DOMAIN}")
+print(f"MAILGUN_RECIPIENT: {MAILGUN_RECIPIENT}")
+print(f"NEED_AMOUMT: {NEED_AMOUMT}")
